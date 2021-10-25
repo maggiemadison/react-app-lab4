4d32de36e139f5171e31d3007181ef0e91e5cd3d
Melissa McGuire <maggiemadison1221@gmail.com>
2021-10-24 20:39:06 -0500

    adding addition jsx and a react fragment

--- src/App.js
+++ src/App.js
@@ -2,7 +2,17 @@ import './App.css';
 
 const App = () => {
   return (
+    <>
     <h1>Hello Class! ! ! </h1>
+
+    <h2>Hello Juan! ! ! </h2>
+
+    <h2>Hello Melanie! ! ! </h2>
+
+    <h2>Hello Sasha! ! ! </h2>
+
+    <h2>Hello Chris! ! ! </h2>
+    </>
   );
 }
 
