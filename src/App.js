a74b2eec18e714e59f3f694678d8d9c160385881
Melissa McGuire <maggiemadison1221@gmail.com>
2021-10-24 20:46:29 -0500

    Adding a greeting component

--- src/App.js
+++ src/App.js
@@ -1,18 +1,9 @@
 import './App.css';
+import Greeting from './Greeting';
 
 const App = () => {
   return (
-    <>
-    <h1>Hello Class! ! ! </h1>
-
-    <h2>Hello Juan! ! ! </h2>
-
-    <h2>Hello Melanie! ! ! </h2>
-
-    <h2>Hello Sasha! ! ! </h2>
-
-    <h2>Hello Chris! ! ! </h2>
-    </>
+    <Greeting />
   );
 }
 
---
+++ src/Greeting.js
@@ -0,0 +1,20 @@
+import react from "react";
+
+const Greeting = () => 
+{
+    return (
+        <>
+            <h1>Hello Class! ! ! </h1>
+
+            <h2>Hello Juan! ! ! </h2>
+
+            <h2>Hello Melanie! ! ! </h2>
+
+            <h2>Hello Sasha! ! ! </h2>
+
+            <h2>Hello Chris! ! ! </h2>
+        </>
+
+    )
+}
+export default Greeting;
