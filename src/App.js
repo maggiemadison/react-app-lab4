233faeffd376bff867a11af77f7c8a58651de59e
Melissa McGuire <maggiemadison1221@gmail.com>
2021-10-24 21:15:00 -0500

    added more props and demo'd  boolean short circuit

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { Greeting } from './Greeting';
 const App = () => {
   return (
     <>
-      <Greeting name="Adv Cloud Class" favoriteNumber={12}/>
+      <Greeting name="Adv Cloud Class" favoriteNumber={12} favoriteColors = {[ "Red", "Lime Green", "Orange"]} />
       <Greeting name="Juan" favoriteNumber={76}/>
       <Greeting name="Savannah" favoriteNumber={16}/>
       <Greeting name="Chris"favoriteNumber={27}/>
--- src/Greeting.js
+++ src/Greeting.js
@@ -7,8 +7,16 @@ export const Greeting = (props) =>
     return (
         <>
             <h2>Hello {props.name} ! ! ! </h2>
+            
             <h3>{props.name}'s favorite number is {props.favoriteNumber}.</h3>
+            
             <h3>{props.name}'s favorite number plus 10 is {props.favoriteNumber +10}.</h3>
+            
+            <h3>{props.name}'s favorite colors are:
+                <ul>
+                    {props.favoriteColors && props.favoriteColors.map(color => <li>{color}</li>)}
+                </ul>
+            </h3>    
             
         </>
 
